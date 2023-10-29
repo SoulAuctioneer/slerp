d4cc0e433d78b4fd56360ee73d312010efe61f67
Ash <ashe@datasightusa.com>
2023-10-29 01:30:19 -0700

    bug

--- drink.py
+++ drink.py
@@ -2,5 +2,5 @@ class Drink:
     def __init__(self, name, rgb, cmyt, page_function):
         self.name = name
         self.rgb = rgb
-        self.cmyt = cmyt,
+        self.cmyt = cmyt
         self.page_function = page_function
