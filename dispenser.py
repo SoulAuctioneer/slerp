5a3a1b91d457eed939c67b893d33ae4897a5aa15
Ash <ashe@datasightusa.com>
2023-10-28 18:35:40 -0700

    Making the dispenser generic, able to take CMYE

--- dispenser.py
+++ dispenser.py
@@ -1,44 +1,82 @@
-from gpiozero import Motor
+from gpiozero import Motor, Device
+from gpiozero.pins.mock import MockFactory, MockPWMPin
 import event_scheduler
 import platform
+from settings import *
 
 class Dispenser:
     def __init__(self):
-        # Only enable on raspberry pi
-        print(platform.system())
-        print(platform.machine())
-        self.enabled = True # platform.system() == 'Linux' and platform.machine() == 'armv7l'
-        self.pump_cyan = None
-        self.pump_magenta = None
-        self.arduino = None
-        self.motor_pin = 18
+
+        print('Initializing GPIO communication')
+
+        # Mock if not on raspberry pi
+        if not platform.system() == 'Linux' and not platform.machine() == 'aarch64':
+            print('gpiozero is disabled')
+            Device.pin_factory = MockFactory(pin_class=MockPWMPin)
+
         self.event_scheduler = event_scheduler.EventScheduler()
 
-        if self.enabled:
-            print('Initializing GPIO communication')
-            self.pump_cyan = Motor(24, 23)
-            self.pump_magenta = Motor(22, 27)
-        else:
-            print('gpiozero is disabled')
 
+    # Execute any events due
     def update(self):
         self.event_scheduler.execute_due()
 
-    def dispense(self, drink):
+    # Dispense a drink with the given amounts of Cyan / Magenta / Yellow / Extra, from 0 to 10 
+    def dispense(self, cyan, magenta, yellow, extra):
+
         print('Dispensing drink')
-        if not self.enabled:
-            return
-        self.event_scheduler.schedule(0, self.pump_cyan.forward)
-        self.event_scheduler.schedule(0, self.pump_magenta.forward)
-        self.event_scheduler.schedule(4, self.pump_cyan.stop)
-        self.event_scheduler.schedule(4, self.pump_magenta.stop)
-        for i in range(10):
-            self.event_scheduler.schedule(4 + i * 0.38, self.pump_cyan.forward)
-            self.event_scheduler.schedule(4 + i * 0.38 + 0.33, self.pump_cyan.stop)
-            self.event_scheduler.schedule(4 + i * 0.38 + 0.38, self.pump_magenta.forward)
-            self.event_scheduler.schedule(4 + i * 0.38 + 0.38 + 0.33, self.pump_magenta.stop)
-        self.event_scheduler.schedule(12, self.pump_cyan.backward)
-        self.event_scheduler.schedule(12, self.pump_magenta.backward)
-        self.event_scheduler.schedule(17, self.pump_cyan.stop)
-        self.event_scheduler.schedule(17, self.pump_magenta.stop)
+
+        # Initialize the pump pins
+        self.pump_cyan = Motor(PUMP_CYAN_OUT, PUMP_CYAN_IN)
+        self.pump_magenta = Motor(PUMP_MAGENTA_OUT, PUMP_MAGENTA_IN)
+        self.pump_yellow = Motor(PUMP_YELLOW_OUT, PUMP_YELLOW_IN)
+        self.pump_extra = Motor(PUMP_EXTRA_OUT, PUMP_EXTRA_IN)
+
+        # Prime all liquids to the top of the collector
+        self.pump_cyan.forward
+        self.pump_magenta.forward
+        self.pump_yellow.forward
+        self.pump_extra.forward
+        self.event_scheduler.schedule(DISPENSER_PRIME_DURATION, self.pump_cyan.stop)
+        self.event_scheduler.schedule(DISPENSER_PRIME_DURATION, self.pump_magenta.stop)
+        self.event_scheduler.schedule(DISPENSER_PRIME_DURATION, self.pump_yellow.stop)
+        self.event_scheduler.schedule(DISPENSER_PRIME_DURATION, self.pump_extra.stop)
+
+        # Iterate up to max times, scheduling liquid to pump if more of its color is still needed
+        timer = 0
+        for i in range(DISPENSER_MAX_SQUIRTS):
+            if cyan > i:
+                self.event_scheduler.schedule(timer, self.pump_cyan.forward)
+                timer += DISPENSER_SQUIRT_DURATION
+                self.event_scheduler.schedule(timer, self.pump_cyan.stop)
+                timer += DISPENSER_SQUIRT_REST_DURATION
+            if magenta > i:
+                self.event_scheduler.schedule(timer, self.pump_magenta.forward)
+                timer += DISPENSER_SQUIRT_DURATION
+                self.event_scheduler.schedule(timer, self.pump_magenta.stop)
+                timer += DISPENSER_SQUIRT_REST_DURATION
+            if yellow > i:
+                self.event_scheduler.schedule(timer, self.pump_yellow.forward)
+                timer += DISPENSER_SQUIRT_DURATION
+                self.event_scheduler.schedule(timer, self.pump_yellow.stop)
+                timer += DISPENSER_SQUIRT_REST_DURATION
+            if extra > i:
+                self.event_scheduler.schedule(timer, self.pump_extra.forward)
+                timer += DISPENSER_SQUIRT_DURATION
+                self.event_scheduler.schedule(timer, self.pump_extra.stop)
+                timer += DISPENSER_SQUIRT_REST_DURATION
+
+        # Suck all the liquids back into the reservoir
+        timer += DISPENSER_SUCK_WAIT_DURATION
+        self.event_scheduler.schedule(timer, self.pump_cyan.backward)
+        self.event_scheduler.schedule(timer, self.pump_magenta.backward)
+        self.event_scheduler.schedule(timer, self.pump_yellow.backward)
+        self.event_scheduler.schedule(timer, self.pump_extra.backward)
+
+        # Stop sucking
+        timer += DISPENSER_SUCK_DURATION
+        self.event_scheduler.schedule(timer, self.pump_cyan.stop)
+        self.event_scheduler.schedule(timer, self.pump_magenta.stop)
+        self.event_scheduler.schedule(timer, self.pump_yellow.stop)
+        self.event_scheduler.schedule(timer, self.pump_extra.stop)
 
--- settings.py
+++ settings.py
@@ -19,3 +19,26 @@ DARK_GREY = (32, 32, 32)
 # Font properties
 BUTTON_FONT_SIZE = 48
 BUTTON_FONT_FACE = 'assets/PeaberryMono.ttf'
+
+# Pump GPIO pins
+PUMP_CYAN_OUT = 24
+PUMP_CYAN_IN = 23
+PUMP_MAGENTA_OUT = 22
+PUMP_MAGENTA_IN = 27
+PUMP_YELLOW_OUT = 8
+PUMP_YELLOW_IN = 0
+PUMP_EXTRA_OUT = 0
+PUMP_EXTRA_IN = 0
+
+# Max number of squirts of liquid that can be produced from each pump per drink
+DISPENSER_MAX_SQUIRTS = 10
+# Time to prime liquids from reservoir to top of collector
+DISPENSER_PRIME_DURATION = 5
+# Time to pump for a single squirt
+DISPENSER_SQUIRT_DURATION = 0.38
+# Time to rest between squirts
+DISPENSER_SQUIRT_REST_DURATION = 0.33
+# Time to wait after all squirts are done before sucking back
+DISPENSER_SUCK_WAIT_DURATION = 12
+# Time to suck back into reservoir
+DISPENSER_SUCK_DURATION = 5
