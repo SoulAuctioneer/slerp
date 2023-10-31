e4f5d022eab080d35c15435ad72175c1544e01fd
Ash <ashe@datasightusa.com>
2023-10-31 01:14:55 -0700

    menu tweak

--- main_loop.py
+++ main_loop.py
@@ -134,14 +134,14 @@ class MainLoop:
 
     def page_admin(self):
         self.set_buttons([
-            Button(self.screen, pygame.Rect(50, 20, 570, 70), 'RESTART', (50, 255, 50), self.page_start),
+            Button(self.screen, pygame.Rect(50, 25, 570, 70), 'RESTART', (50, 255, 50), self.page_start),
             Button(self.screen, pygame.Rect(50, 110, 570, 70), 'DRINKS SCREEN', (50, 50, 255), self.show_drink_buttons),
-            Button(self.screen, pygame.Rect(50, 200, 570, 70), 'TEST CYAN', (0, 255, 255), lambda: self.dispenser.test('cyan')),
-            Button(self.screen, pygame.Rect(50, 290, 570, 70), 'TEST MAGENTA', (255, 0, 255), lambda: self.dispenser.test('magenta')),
-            Button(self.screen, pygame.Rect(50, 380, 570, 70), 'TEST YELLOW', (255, 255, 0), lambda: self.dispenser.test('yellow')),
-            Button(self.screen, pygame.Rect(50, 470, 570, 70), 'TEST TRANSPARENT', (150, 150, 165), lambda: self.dispenser.test('transparent')),
-            Button(self.screen, pygame.Rect(50, 560, 570, 70), 'TEST PRIMING', (180, 128, 128), self.dispenser.test_prime),
-            Button(self.screen, pygame.Rect(50, 560, 570, 70), 'EXIT', (255, 50, 50), self.stop_loop)
+            Button(self.screen, pygame.Rect(50, 195, 570, 70), 'TEST CYAN', (0, 255, 255), lambda: self.dispenser.test('cyan')),
+            Button(self.screen, pygame.Rect(50, 280, 570, 70), 'TEST MAGENTA', (255, 0, 255), lambda: self.dispenser.test('magenta')),
+            Button(self.screen, pygame.Rect(50, 365, 570, 70), 'TEST YELLOW', (255, 255, 0), lambda: self.dispenser.test('yellow')),
+            Button(self.screen, pygame.Rect(50, 450, 570, 70), 'TEST TRANSPARENT', (150, 150, 165), lambda: self.dispenser.test('transparent')),
+            Button(self.screen, pygame.Rect(50, 535, 570, 70), 'TEST PRIMING', (180, 128, 128), self.dispenser.test_prime),
+            Button(self.screen, pygame.Rect(50, 620, 570, 70), 'EXIT', (255, 50, 50), self.stop_loop)
         ])
 
     # Handle pygame events
