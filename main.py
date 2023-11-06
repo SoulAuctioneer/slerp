4d1d6a52369b8b1f108bb21116398400f733e13b
Ash <ashe@datasightusa.com>
2023-11-05 18:27:14 -0800

    Pin tweaks

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+
+import logging
 from main_loop import MainLoop
 
 if __name__ == "__main__":
+
+    logging.basicConfig(level=logging.DEBUG,
+                        handlers=[logging.FileHandler('log.txt', 'w', 'utf-8'),
+                                  logging.StreamHandler()])
 
     # Initialize the looper
     main_loop = MainLoop()
