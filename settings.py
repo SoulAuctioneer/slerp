3cb065f616ba14b38a37415d0e6495c39b694044
Ash <ashe@datasightusa.com>
2023-11-11 01:05:55 -0800

    PIN REORDERING

--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@
 SCREEN_WIDTH, SCREEN_HEIGHT = 1280, 720
 
 # Fullscreen or windowed
-IS_FULLSCREEN = False
+IS_FULLSCREEN = True
 
 # Background image
 BG_IMAGE = "assets/background-logo-1280x720.png"
@@ -34,19 +34,19 @@ PUMP_TRANSPARENT_IN = 15 # Green
 PUMP_TRANSPARENT_SPEED = 18 # Purple
 
 # Top Right - Looking from rear
-PUMP_CYAN_OUT = 17 # Yellow # NOTE FLIPPED
-PUMP_CYAN_IN = 27 # Orange # NOTE FLIPPED
-PUMP_CYAN_SPEED = 13 # Brown
+PUMP_MAGENTA_OUT = 17 # Yellow # NOTE FLIPPED
+PUMP_MAGENTA_IN = 27 # Orange # NOTE FLIPPED
+PUMP_MAGENTA_SPEED = 13 # Brown
 
 # Bottom Left - Looking from rear
-PUMP_MAGENTA_OUT = 25 # Yellow 
-PUMP_MAGENTA_IN = 24 # Orange
-PUMP_MAGENTA_SPEED = 12 # Brown
+PUMP_YELLOW_OUT = 25 # Yellow 
+PUMP_YELLOW_IN = 24 # Orange
+PUMP_YELLOW_SPEED = 12 # Brown
 
 # Top Left - Looking from rear
-PUMP_YELLOW_OUT = 11 # Blue # NOTE FLIPPED
-PUMP_YELLOW_IN = 9 # Green # NOTE FLIPPED
-PUMP_YELLOW_SPEED = 19 # Purple
+PUMP_CYAN_OUT = 11 # Blue # NOTE FLIPPED
+PUMP_CYAN_IN = 9 # Green # NOTE FLIPPED
+PUMP_CYAN_SPEED = 19 # Purple
 
 # Time to prime liquids from reservoir to top of collector. Will be different if there's a variance in tube length
 PUMP_CYAN_PRIME_DURATION = 3.85
