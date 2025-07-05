9c26e079fe94e7154cbeea7717fa93a221edc5f3
Ash <ash.eldritch@gmail.com>
2025-07-05 14:54:27 -0700

    erk

--- slerp.sh
+++ slerp.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 cd ~/slerp
+# Force SDL to use the ALSA audio driver to prevent conflicts with PulseAudio
+# when running as root.
+export SDL_AUDIODRIVER=alsa
 # Set the SDL_AUDIODEV environment variable to force Pygame/SDL to use the
 # USB audio device (card 3, device 0 from `aplay -l`).
 # This is needed because the script is run with sudo, which doesn't use the
