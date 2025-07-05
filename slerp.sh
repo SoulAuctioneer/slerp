7ddc4db77be963556fbb4f0b6aa7526c4b562bcf
Ash <ash.eldritch@gmail.com>
2025-07-05 14:53:09 -0700

    set usb audio

--- slerp.sh
+++ slerp.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 cd ~/slerp
-sudo .venv/bin/python ./main.py
+# Set the SDL_AUDIODEV environment variable to force Pygame/SDL to use the
+# USB audio device (card 3, device 0 from `aplay -l`).
+# This is needed because the script is run with sudo, which doesn't use the
+# current user's audio settings. 'plughw:1,0' is a common
+export SDL_AUDIODEV=plughw:3,0
+sudo -E .venv/bin/python ./main.py
